91942fc66a5d546f0ef8c11ec2cbf161548511ca
Varchronos <lcs2020062@iiitl.ac.in>
2023-11-24 10:38:02 +0530

    created form validation for people_input

--- src/components/Utils/Input_util.jsx
+++ src/components/Utils/Input_util.jsx
@@ -5,28 +5,37 @@ const Input_util = (props) => {
 
     const [value, setValue] = useState('');
 
-    const changeHandler = (identifier, event)=>{
+    const changeHandler = (identifier, event) => {
         setValue(event)
     }
+    let value_cant_be_zero_label = <></>;
+    useEffect(() => {
+        const data = { identifier: props.identifier, value: value }
 
-    useEffect(()=>{
-        const data = {identifier: props.identifier, value:value}
+        if (props.identifier === 'people' && value === '0') {
+            console.log('people cannot be zero')
+            return;
+        }
+        // console.log(value)
         props.onDataRecieve(data);
         // console.log(value)
-    },[value])
-    
-    
+    }, [value])
+
+
 
     return (
         <>
-        <div className={classes.input_group}>
-                    <label htmlFor="bill">{props.children}</label>
-                    <input type="number" placeholder='$'
-                    value={value}
-                    onChange={(event)=>changeHandler(props.identifier, event.target.value)}
-                    //  onChange={(event)=>focusHandeler(props.identifier, event.target.value)}
-                     />
+            <div className={classes.input_group}>
+                <div className={classes.label_group}>
+                    <label>{props.children}</label>
+                    {props.identifier === 'people' && value === '0' && <label className={classes.error_label}>can't be zero</label>}
                 </div>
+                <input className ={`${classes.input} ${(props.identifier==='people' && value==='0')?classes.error_input:''}`} type="number" placeholder='$'
+                    value={value}
+                    onChange={(event) => changeHandler(props.identifier, event.target.value)}
+                //  onChange={(event)=>focusHandeler(props.identifier, event.target.value)}
+                />
+            </div>
         </>
     )
 }
